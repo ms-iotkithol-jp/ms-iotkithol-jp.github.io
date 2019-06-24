abd614b78d36d45fb4354bf54a843a1c1c65817c
ms-iotkithol-jp <ms-iotkithol-jp@outlook.jp>
2019-06-24 14:42:53 +0900

    Updata some missing parts

--- test/signalrservice/scripts/databysignalr.js
+++ test/signalrservice/scripts/databysignalr.js
@@ -1,63 +1,54 @@
-< !DOCTYPE html >
-    <
-    html >
+$(document).ready(function() {
+    const apiBaseUrl = "https://egsignalrservicehub20190620.azurewebsites.net ";
 
-    <
-    head >
-    <
-    meta charset = "utf-8" / >
-    <
-    title > Test < /title> <
-    script type = "text/javascript"
-src = "https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" > < /script> <
-    script src = "https://cdn.jsdelivr.net/npm/@aspnet/signalr@1.1.2/dist/browser/signalr.js" > < /script> <
-    script type = "text/javascript"
-src = "scripts/databysignalr.js" > < /script> <
-    /head>
+    getConnectionInfo().then(function(info) {
+        let accessToken = info.accessToken;
 
-<
-body >
-    <
-    h1 > Hello < /h1> <
-    /body>
+        const options = {
+            accessTokenFactory: function() {
+                if (accessToken) {
+                    const _accessToken = accessToken;
+                    accessToken = null;
+                    return _accessToken;
+                } else {
+                    return getConnectionInfo().then(function(info) {
+                        return info.accessToken;
+                    });
+                }
+            }
+        };
 
-<
-/html>             });
-}
-}
-};
+        const connection = new signalR.HubConnectionBuilder()
+            .withUrl(info.uri, options)
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
 
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl(info.uri, options)
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
-connection.on("SendData", function(obj) {
-    message = obj;
-    x = message.x;
-    y = message.y;
-    console.log('x=' + x + 'y=' + y);
-});
+        connection.on("SendData", function(obj) {
+            message = obj;
+            x = message.x;
+            y = message.y;
+            console.log('x=' + x + 'y=' + y);
+        });
 
 
-connection.onclose(function() {
-    console.log('disconnected');
-    setTimeout(function() { startConnection(connection); }, 2000);
-});
+        connection.onclose(function() {
+            console.log('disconnected');
+            setTimeout(function() { startConnection(connection); }, 2000);
+        });
 
-console.log('connecting...');
-connection.start().then(() => data.ready = true)
-    .catch(console.error);
-});
-
-function getConnectionInfo() {
-    return $.post({
-        url: '${apiBaseUrl}/api/negotiate',
-        data: {}
-    }).done(function(resp, textStatus, jqXHR) {
-        return resp.data;
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
+        console.log('connecting...');
+        connection.start().then(() => data.ready = true)
+            .catch(console.error);
     });
-}
+
+    function getConnectionInfo() {
+        return $.post({
+            url: '${apiBaseUrl}/api/SignalRInfo',
+            data: {}
+        }).done(function(resp, textStatus, jqXHR) {
+            return resp.data;
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        });
+    }
 });
